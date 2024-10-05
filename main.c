726eee190f60d37a6a362c7266f089467729e3ed
bolsakova <tanjutik162@gmail.com>
2024-10-05 16:56:57 +0200

    created the structure for main file of the game, also with checking argument passed, which is the map

--- main.c
+++ main.c
@@ -6,10 +6,47 @@
 /*   By: tbolsako <tbolsako@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:30:36 by tbolsako          #+#    #+#             */
-/*   Updated: 2024/10/03 16:30:43 by tbolsako         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:38:47 by tbolsako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static void	check_args(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		ft_printf("Error: Invalid number of arg\n");
+		exit(EXIT_FAILURE);
+	}
+	if (!ft_strnstr(av[1], ".ber", ft_strlen(av[1])))
+	{
+		ft_printf("Error: Invalid file extension\n");
+		exit(EXIT_FAILURE);
+	}
+}
 
+static void	game_loop(void *param)
+{
+	t_game	*game;
+
+	game = (t_game *)param;
+	render_map(game);
+}
+
+int	main(int ac, char *av[])
+{
+	t_game		game;
+	t_render	render;
+
+	check_args(ac, av);
+	if (!init_game(&game, &render, av[1]))
+	{
+		ft_printf("Error: Failed to initialize the game\n");
+		exit(EXIT_FAILURE);
+	}
+	mlx_loop_hook(render.mlx, (void *)&game_loop, &game);
+	mlx_loop(render.mlx);
+	cleanup(&game, &render);
+	return (EXIT_SUCCESS);
+}
--- so_long.h
+++ so_long.h
@@ -6,39 +6,59 @@
 /*   By: tbolsako <tbolsako@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 16:22:58 by tbolsako          #+#    #+#             */
-/*   Updated: 2024/10/03 16:35:41 by tbolsako         ###   ########.fr       */
+/*   Updated: 2024/10/05 16:36:04 by tbolsako         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-// minilibx library
+# include "./get_next_line/get_next_line.h"
+# include "./libft/libft.h"
+# include "./printf/ft_printf.h"
 # include "MLX42/include/MLX42/MLX42.h"
-
 // file control options library
 # include <fcntl.h>
-
-// standard libraries
 # include <stdlib.h>
 # include <unistd.h>
 
+# define BUFFER_SIZE 1024
 // define the size of each tile in pixels
 # define TILE_SIZE 32
-
 // the player's starting position
 # define PLAYER 'P'
-
 // a map exit
 # define EXIT 'E'
-
 // collectible items
 # define COLLECTIBLE 'C'
-
 // walls
 # define WALL '1'
-
 // an empty space
 # define EMPTY '0'
+
+// game's structure
+typedef struct s_game
+{
+	char	**map_data;
+	int		map_length;
+	int		map_height;
+	int		player_pos_x;
+	int		player_pos_y;
+	int		collectibles;
+	int		collected;
+	int		game_over;
+	int		steps;
+	void	*img_player;
+	void	*img_wall;
+	void	*img_collectibles;
+	void	*img_exit;
+	void	*img_empty;
+}			t_game;
+
+typedef struct s_render
+{
+	void	*mlx;
+	void	*win;
+}			t_render;
 
 #endif
